04d604a96295bf9515d2dc499f3f292f25650188
Michelle Feng <michellefeng@michelles-mbp-2.lan>
2019-01-16 00:42:48 -0800

    Deleted unused scripts

--- scripts/extractvertices.m
+++
@@ -1,8 +0,0 @@
-function vertices=extractvertices(annotated)
-    vertices={};
-    for i=[1:length(annotated)]
-        formalsum=get(annotated(i),'Second');
-        simplex=edu.stanford.math.plex4.utility.FormalSumUtility.extractActiveBasisElements(formalsum);
-        vertices{i}= sortvertices(edu.stanford.math.plex4.utility.FormalSumUtility.extractVertices(simplex));
-    end
-end
--- scripts/run_all_vr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Datafile='../full-list'
-
-for COUNTY in $(cat $Datafile); do
-  ./workflow.sh hill $COUNTY vr
-  ./workflow.sh trump $COUNTY vr
-done
